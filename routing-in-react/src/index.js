289f99782a7bca18c1353c1831fcb8a790a65373
Shubham Giri <shubhamgiri7631.sg@gmail.com>
2024-08-26 23:00:45 +0530

    handling dynamic routes

--- routing-in-react/src/index.js
+++ routing-in-react/src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Contact from "./components/Contact";
 import About from "./components/About";
 import Header from "./components/Header";
+import SingleArticle from "./components/SingleArticle";
 
 ReactDOM.render(
   <BrowserRouter>
@@ -14,6 +15,7 @@ ReactDOM.render(
       <Route path="/contact" element={<Contact />} />
       <Route path="/about" element={<About />} />
       <Route path="/" element={<App />} />
+      <Route path="/post/:id" Component={SingleArticle} />
     </Routes>
   </BrowserRouter>,
   document.querySelector("#root")
